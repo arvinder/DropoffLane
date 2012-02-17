412ec939962a9c1471e2766c79f98b612d5b428b
Adam McDonald <mcdonald.adam@gmail.com>
2012-02-16 17:14:26 -0800

    Initial Commit

---
+++ DropoffLane/DLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLAppDelegate.h
+//  DropoffLane
+//
+//  Created by Adam McDonald on 2/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
